11595a3a652941acb894a665f184448cedafeeca
Luc Lapierre <losted@losted.net>
2015-09-23 01:12:12 -0400

    Create sublime.sh

---
+++ app/sublime.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# WARNING: Remove old directories and symlink new ones from Dropbox
+
+# Remove old directories
+cd /opt/homebrew-cask/Caskroom/sublime-text3/*/Sublime\ Text.app/Contents/MacOS/
+rm -rf Packages/
+rm -rf Installed\ Packages/
+
+# Symlink new directories from Dropbox
+ln -s ~/Dropbox/Sublime\ Text\ 3/Packages/
+ln -s ~/Dropbox/Sublime\ Text\ 3/Installed\ Packages/
