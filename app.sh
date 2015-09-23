c4e2984a16d3287a3a05951d69d438f900bc46f3
Luc Lapierre <losted@losted.net>
2015-09-22 21:20:58 -0400

    Update app.sh

--- app.sh
+++ app.sh
@@ -8,21 +8,6 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# XCode Command Line Tools
-if [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; then
-    xcode-select --install &> /dev/null
-    # Wait until the XCode Command Line Tools are installed
-    while [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; do
-        sleep 5
-    done
-	xcode-select -p &> /dev/null
-	if [ $? -eq 0 ]; then
-        # Prompt user to agree to the terms of the Xcode license
-        # https://github.com/alrra/dotfiles/issues/10
-       sudo xcodebuild -license
-   fi
-fi
-
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
