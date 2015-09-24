87f5c64572126cab5966864a84790b6108ff8e2f
Luc Lapierre <losted@losted.net>
2015-09-23 20:12:46 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,11 +3,12 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenk
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install jenkins
 
-# Disable firewall rules
-firewall-cmd --zone=public --add-port=8080/tcp --permanent
-firewall-cmd --zone=public --add-service=http --permanent
-firewall-cmd --reload
-firewall-cmd --list-all
-
-# Open port 8080 
+# Open port 8080 in the firewall
 iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+
+# Save and restart iptables
+service iptables save
+service iptables restart
+
+# Start jenkins
+service jenkins start
