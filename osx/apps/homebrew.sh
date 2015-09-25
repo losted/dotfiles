cb80e6bdc9b4e9d524fe47b9c0173696eed9f2e9
Luc Lapierre <losted@losted.net>
2015-09-24 21:26:29 -0400

    Create homebrew.sh

---
+++ osx/apps/homebrew.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Check for Homebrew installation
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+
+  # Install homebrew
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+fi
+
+# Update homebrew formaulaes
+brew update
+
+# Install some packages
+brew install wget ssh-copy-id coreutils hub trash tmux reattach-to-user-namespace
+
+# Install homebrew cask
+brew tap phinze/homebrew-cask
+brew tap caskroom/versions
+brew install brew-cask
