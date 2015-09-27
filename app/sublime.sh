dd7c0de8ba8c1c60a01857e86950aec3cd32027a
Luc Lapierre <losted@losted.net>
2015-09-26 23:14:28 -0400

    Update sublime.sh

--- app/sublime.sh
+++ app/sublime.sh
@@ -9,15 +9,5 @@ wget http://packagecontrol.io/Package%20Control.sublime-package -P "$HOME/Librar
 composer global require fabpot/php-cs-fixer
 
 # Link preferences files
-# rm -f "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-# ln -s "$(pwd)/$(dirname $0)/sublimetext3/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-# ln -s "$(pwd)/$(dirname $0)/sublimetext3/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
-
-# Link theme file
-# ln -s "$(pwd)/$(dirname $0)/sublimetext3/Default.sublime-theme" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Default.sublime-theme"
-
-# Link snippets
-# ln -s "$(pwd)/$(dirname $0)/sublimetext3/snippets" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Snippets"
-
-# Add shortcut to Dock
-# defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$HOME/Applications/Sublime Text.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>
+rm -f "$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
+ln -s sublimetext3/User/ "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
