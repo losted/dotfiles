c1cb4e00a60b1b514965ece4243bd904d8473381
Luc Lapierre <luc@hydrogene.io>
2017-09-06 12:38:27 -0400

    update arch things

---
+++ arch/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -sf ~/code/dotfiles/arch/.aliases ~/.aliases
+ln -sf ~/code/dotfiles/arch/.functions ~/.functions
+ln -sf ~/code/dotfiles/arch/.zshrc ~/.zshrc
+ln -sf ~/code/dotfiles/arch/.gitignore ~/.gitignore
